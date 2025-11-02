29a1c34e30e512e6cdfc538049fe7bb29873d97f
pewpil <pewpil@proton.me>
2025-11-02 10:51:34 +0800

    writing features initiated

--- src/components/home/BrowseCar.tsx
+++ src/components/home/BrowseCar.tsx
@@ -12,57 +12,7 @@ export default function BrowseCar(): ReactElement {
 						<img src="car.png" id={style.model} />
 					</div>
 					<div id={style.next_car}>
-						<svg
-							width="44"
-							height="49"
-							viewBox="0 0 44 49"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-						>
-							<g filter="url(#filter0_d_2_191)">
-								<path
-									d="M38.5 17.7269C40.5 18.8816 40.5 21.7684 38.5 22.9231L8.5 40.2436C6.5 41.3983 4 39.9549 4 37.6455L4 3.00447C4 0.695069 6.5 -0.748305 8.5 0.406396L38.5 17.7269Z"
-									fill="#E2E2E2"
-								/>
-							</g>
-							<defs>
-								<filter
-									id="filter0_d_2_191"
-									x="0"
-									y="0"
-									width="44"
-									height="48.65"
-									filterUnits="userSpaceOnUse"
-									color-interpolation-filters="sRGB"
-								>
-									<feFlood flood-opacity="0" result="BackgroundImageFix" />
-									<feColorMatrix
-										in="SourceAlpha"
-										type="matrix"
-										values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-										result="hardAlpha"
-									/>
-									<feOffset dy="4" />
-									<feGaussianBlur stdDeviation="2" />
-									<feComposite in2="hardAlpha" operator="out" />
-									<feColorMatrix
-										type="matrix"
-										values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-									/>
-									<feBlend
-										mode="normal"
-										in2="BackgroundImageFix"
-										result="effect1_dropShadow_2_191"
-									/>
-									<feBlend
-										mode="normal"
-										in="SourceGraphic"
-										in2="effect1_dropShadow_2_191"
-										result="shape"
-									/>
-								</filter>
-							</defs>
-						</svg>
+					<img src="next_car.svg" />
 					</div>
 				</div>
 			</div>
